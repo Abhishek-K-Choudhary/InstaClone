251a6a36fec8155ceb5bd88882a43eb3f6327978
Abhishek Choudhary <abhishek.choudhary2019@gift.edu.in>
2022-11-28 09:51:41 +0530

    creates logic and UI for posting the new post securely

--- client/src/components/screens/CreatePost.js
+++ client/src/components/screens/CreatePost.js
@@ -1,6 +1,63 @@
-import React from "react";
+import React,{useState, useEffect } from "react";
+import { useNavigate } from 'react-router-dom';
+import M from 'materialize-css';
+
 
 const CreatePost = () =>{
+    const navigate = useNavigate()
+    const [title, setTitle] = useState("")
+    const [body, setBody] = useState("")
+    const [image, setImage] = useState("")
+    const [url,setUrl] = useState("")
+
+    useEffect(()=>{
+        if(url){
+        fetch("/createpost",{
+            method:"post",
+            headers:{
+                "Content-Type":"application/json",
+                "Authorization":"Bearer "+localStorage.getItem("jwt")
+            },
+            body:JSON.stringify({
+                title,
+                body,
+                pic:url
+            })
+        })
+        .then(res=>res.json())
+        .then(data=>{
+
+            if(data.error){
+                M.toast({html: data.error, classes:'#ff1744 red accent-3'})
+            }
+            else{
+                M.toast({html: "Posted Successfully!",classes:'#2e7d32 green darken-3'})
+                navigate('/');
+            }
+        }).catch(err=>{
+            console.log(err)
+        })
+    }
+    },[url])
+    
+
+    const postDetails = () =>{
+        const data = new FormData()
+        data.append("file", image)
+        data.append("upload_preset", "insta-clone")
+        data.append("cloud_name", "di4eqhwzy")
+        fetch("https://api.cloudinary.com/v1_1/di4eqhwzy/image/upload",{
+            method:"post",
+            body:data
+        })
+        .then(res=>res.json())
+        .then(data=>{
+            setUrl(data.url)
+        }).catch(err=>{
+            console.log(err)
+        })
+    }
+
     return(
         <div className="card input-field" style={{
             margin:"30px auto",
@@ -8,18 +65,28 @@ const CreatePost = () =>{
             padding:"20px",
             textAlign:"center"
         }}>
-            <input type="text" placeholder="Title" />
-            <input type="text" placeholder="Body" />
+            <input type="text" 
+            placeholder="Title"
+            value={title}
+            onChange={(e)=>setTitle(e.target.value)} 
+            />
+            <input type="text" 
+            placeholder="Body" 
+            value={body}
+            onChange={(e)=>setBody(e.target.value)}
+            />
             <div className="file-field input-field">
             <div className="btn #42a5f5 blue darken-1">
                 <span>Upload the image</span>
-                <input type="file" />
+                <input type="file" onChange={(e)=>setImage(e.target.files[0])} />
             </div>
             <div className="file-path-wrapper">
                 <input className="file-path validate" type="text" />
             </div>
             </div>
-            <button className="btn waves-effect waves-light #42a5f5 blue darken-1">
+            <button className="btn waves-effect waves-light #42a5f5 blue darken-1"
+            onClick={()=>postDetails()}
+            >
                     Submit Post
             </button>
         </div>
--- server/routes/auth.js
+++ server/routes/auth.js
@@ -27,7 +27,7 @@ router.post('/signup', (req,res)=>{
             })
             user.save()
             .then(user=>{
-                res.json({massage:"loged into the database Successfully!"})
+                res.json({massage:"SignedUp Successfully!"})
             })
             .catch(err=>{
                 console.log(err)
@@ -38,7 +38,7 @@ router.post('/signup', (req,res)=>{
         console.log(err)
     })
 })
-router.post('/signin', (req, res)=>{
+router.post('/login', (req, res)=>{
     const {email, password} = req.body
     if(!email || !password){
         return res.status(422).json({error:"Please Enter valid email or password"})
@@ -46,14 +46,15 @@ router.post('/signin', (req, res)=>{
     User.findOne({email:email})
     .then(savedUser=>{
         if(!savedUser){
-            return res.status(422).json({error:"Invalid email or password"})
+            return res.status(422).json({error:"Invalid Email or Password!"})
         }
         bcrypt.compare(password, savedUser.password)
         .then(doMatch=>{
             if(doMatch){
                 // res.json({massage:"Successfully LoggedIn!"})
                 const token = jwt.sign({_id:savedUser._id},JWT_SECRET)
-                res.json({token})
+                const {_id, name, email} = savedUser
+                res.json({token, user:{_id, name, email}})
             }
             else{
                 return res.status(422).json({error:"Invalid email or password"})
